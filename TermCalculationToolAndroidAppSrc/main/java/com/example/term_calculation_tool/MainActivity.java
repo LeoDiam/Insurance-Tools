33d0e1525f9702d22128559fca727ab2ee1e90df
Leonidas Diamantopoulos <t8190243@aueb.gr>
2021-04-18 09:57:52 +0300

    Add files via upload

---
+++ TermCalculationToolAndroidAppSrc/main/java/com/example/term_calculation_tool/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.term_calculation_tool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText ageT = findViewById(R.id.ageT);
+        EditText savedCapital = findViewById(R.id.capitalT);
+        TextView firstFee = findViewById(R.id.firstFeeT);
+        TextView averageYearsFee = findViewById(R.id.averagetenyearsfeeT);
+        Button calculate = findViewById(R.id.calculateButtonThird);
+        Button close = findViewById(R.id.continueButtonThird);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int age = Integer.valueOf(ageT.getText().toString());
+                int capital = Integer.valueOf(savedCapital.getText().toString());
+                firstFee.setText(String.valueOf(String.format("%.2f", (calculateFirstFee(age,capital)+30))));
+                double averagefirsttenyears = 0.0;
+                for(int i = 0; i<10; i++){
+                    averagefirsttenyears += calculateFirstFee(age+i,capital);
+                }
+                averagefirsttenyears += 30;
+                averagefirsttenyears = averagefirsttenyears/10;
+                averageYearsFee.setText(String.valueOf(String.format("%.2f", (averagefirsttenyears))));
+            }
+        });
+
+
+    }
+    public static double calculateFirstFee(int age, int capital) {
+        if (age == 18) {
+            return 41.31 + capital * 0.000995;
+        } else if (age == 19) {
+            return 41.31 + capital * 0.001115;
+        } else if (age == 20) {
+            return 41.31 + capital * 0.001198;
+        } else if (age == 21) {
+            return 41.31 + capital * 0.00126;
+        } else if (age == 22) {
+            return 41.31 + capital * 0.001301;
+        } else if (age == 23) {
+            return 41.31 + capital * 0.001324;
+        } else if (age == 24) {
+            return 41.31 + capital * 0.001331;
+        } else if (age == 25) {
+            return 41.31 + capital * 0.001323;
+        } else if (age == 26) {
+            return 41.31 + capital * 0.001307;
+        } else if (age == 27) {
+            return 41.31 + capital * 0.001287;
+        } else if (age == 28) {
+            return 41.31 + capital * 0.001266;
+        } else if (age == 29) {
+            return 41.31 + capital * 0.001248;
+        } else if (age == 30) {
+            return 41.31 + capital * 0.001231;
+        } else if (age == 31) {
+            return 41.31 + capital * 0.001218;
+        } else if (age == 32) {
+            return 41.31 + capital * 0.001211;
+        } else if (age == 33) {
+            return 41.31 + capital * 0.001214;
+        } else if (age == 34) {
+            return 41.31 + capital * 0.001235;
+        } else if (age == 35) {
+            return 41.31 + capital * 0.001276;
+        } else if (age == 36) {
+            return 41.31 + capital * 0.001342;
+        } else if (age == 37) {
+            return 41.31 + capital * 0.001431;
+        } else if (age == 38) {
+            return 41.31 + capital * 0.001539;
+        } else if (age == 39) {
+            return 41.31 + capital * 0.001663;
+        } else if (age == 40) {
+            return 41.31 + capital * 0.001797;
+        } else if (age == 41) {
+            return 41.31 + capital * 0.001942;
+        } else if (age == 42) {
+            return 41.31 + capital * 0.002103;
+        } else if (age == 43) {
+            return 41.31 + capital * 0.002276;
+        } else if (age == 44) {
+            return 41.31 + capital * 0.002462;
+        } else if (age == 45) {
+            return 41.31 + capital * 0.00267;
+        } else if (age == 46) {
+            return 41.31 + capital * 0.002911;
+        } else if (age == 47) {
+            return 41.31 + capital * 0.003195;
+        } else if (age == 48) {
+            return 41.31 + capital * 0.003532;
+        } else if (age == 49) {
+            return 41.31 + capital * 0.003926;
+        } else if (age == 50) {
+            return 41.31 + capital * 0.004368;
+        } else if (age == 51) {
+            return 41.31 + capital * 0.004842;
+        } else if (age == 52) {
+            return 41.31 + capital * 0.005331;
+        } else if (age == 53) {
+            return 41.31 + capital * 0.005831;
+        } else if (age == 54) {
+            return 41.31 + capital * 0.006357;
+        } else if (age == 55) {
+            return 41.31 + capital * 0.00694;
+        } else if (age == 56) {
+            return 41.31 + capital * 0.007612;
+        } else if (age == 57) {
+            return 41.31 + capital * 0.008375;
+        } else if (age == 58) {
+            return 41.31 + capital * 0.009239;
+        } else if (age == 59) {
+            return 41.31 + capital * 0.010212;
+        } else if (age == 60) {
+            return 41.31 + capital * 0.011293;
+        }else if(age == 61){
+            return 41.31 + capital * 0.012484;
+        }else if(age == 62){
+            return 41.31 + capital * 0.013775;
+        }else if(age == 63){
+            return 41.31 + capital * 0.015158;
+        }else if(age == 64){
+            return 41.31 + capital * 0.016642;
+        }else if(age == 65){
+            return 41.31 + capital * 0.018233;
+        }else if(age == 66){
+            return 41.31 + capital * 0.019951;
+        }else if(age == 67){
+            return 41.31 + capital * 0.02182;
+        }else if(age == 68){
+            return 41.31 + capital * 0.023869;
+        }else if(age == 69){
+            return 41.31 + capital * 0.026124;
+        }
+        return 0.0;
+    }
+}
